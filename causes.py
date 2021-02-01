c24639219c00428cfdbaaef68d35993bdd2f5002
Søren Ragsdale <sorenr@users.noreply.github.com>
2021-02-01 17:33:43 +0000

    causes.py generates cause counts and cause association percentages

--- causes.py
+++ causes.py
@@ -4,64 +4,94 @@ import operator
 import matplotlib.pyplot as plt
 import numpy
 
-conditions = {}
-categories = {}
+def add_cat(table, category, ndeaths, nmentions):
+    """Add a category to the table."""
+    new_data = (int(ndeaths), int(nmentions))
+    if category in table:
+        table[category] = table[category] + new_data
+    else:
+        table[category] = new_data
 
-with open('causes.csv', newline='') as csvfile:
-    reader = csv.reader(csvfile, delimiter=',', quotechar='"')
-    for row in reader:
-        as_of, wk_start, wk_end, state, category, condition, icd10, age, ndeaths, mentions, flag = row
 
-        # disregard "all ages"
-        if age.lower() != "all ages":
-            continue
+def get_data(fname):
+    """Parse fname and return the condition and category data."""
+    conditions = {}
+    categories = {}
 
-        # disregard no deaths
-        if not ndeaths:
-            continue
+    with open(fname, newline='') as csvfile:
+        reader = csv.reader(csvfile, delimiter=',', quotechar='"')
+        for row in reader:
+            as_of, wk_start, wk_end, state, category, condition, icd10, age, ndeaths, mentions, flag = row
 
-        if state != "US":
-            continue
+            # disregard "all ages"
+            if age.lower() != "all ages":
+                continue
 
-        try:
-            ndeaths = int(ndeaths)
-        except ValueError:
-            continue
+            # disregard no deaths
+            if not ndeaths:
+                continue
 
-        if condition in conditions:
-            conditions[condition] += int(ndeaths)
-        else:
-            conditions[condition] = int(ndeaths)
+            if state != "US":
+                continue
 
-        if category in categories:
-            categories[category] += int(ndeaths)
-        else:
-            categories[category] = int(ndeaths)
+            try:
+                ndeaths = int(ndeaths)
+            except ValueError:
+                continue
 
-d = float(conditions['COVID-19'])
+            add_cat(conditions, condition, ndeaths, mentions)
+            add_cat(categories, category, ndeaths, mentions)
 
-cond_labels = []
-cond_data = []
+    return conditions, categories
 
-for row in sorted(conditions.items(), key=operator.itemgetter(1), reverse=True):
-    print(row[1], row[0])
-    cond_labels.append(row[0])
-    cond_data.append(row[1]/1000)
 
-print()
-print((sum(conditions.values()) - d) / d)
+def make_totals(data, divisor=1000):
+    """Return a ranked list of total deaths."""
+    rv = []
+    for category, vals in data.items():
+        rv.append((vals[0]/divisor, category))
+    return list(sorted(rv, reverse=True))
 
-plt.rcdefaults()
-fig, ax = plt.subplots()
 
-y_pos = numpy.arange(len(cond_data))
+def make_percents(data):
+    """Return a ranked list of COVID percentages."""
+    rv = []
+    for category, vals in data.items():
+        rv.append((100 * vals[0] / vals[1], category))
+    return list(sorted(rv, reverse=True))
 
-ax.barh(y_pos, cond_data, align='center')
-ax.set_yticks(y_pos)
-ax.set_yticklabels(cond_labels)
-ax.invert_yaxis()  # labels read top-to-bottom
-ax.set_xlabel('Fatal Cases (thousands)')
-ax.set_title('SARS-CoV-2 Comorbidities')
 
-plt.subplots_adjust(left=0.4)
-plt.show()
+def plot_data(title, xlabel, data):
+    values, labels = zip(*data)
+    pprint.pprint(data)
+
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+
+    y_pos = numpy.arange(len(labels))
+
+    ax.barh(y_pos, values, align='center')
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(labels)
+    ax.invert_yaxis()  # labels read top-to-bottom
+    ax.set_xlabel(xlabel)
+    ax.set_title(title)
+
+    plt.subplots_adjust(left=0.4)
+    plt.show()
+
+
+if __name__ == "__main__":
+    conditions, categories = get_data('causes.csv')
+
+    totals = make_totals(conditions)
+    plot_data(
+        title='Comorbidities Mentioned in Conjunction With COVID-19 Deaths',
+        xlabel='Total Mentions (thousands)',
+        data=totals)
+
+    percents = make_percents(conditions)
+    plot_data(
+        title='Comorbidities Mentioned in Conjunction With COVID-19 Deaths',
+        xlabel='Case Association (percent)',
+        data=percents)
