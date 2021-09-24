cfc15ad8e0d4ed0e9c4e0f39324373c381c202e1
Søren Ragsdale <sorenr@users.noreply.github.com>
2021-09-24 10:03:44 +0100

    vaers.py updates

--- vaers.py
+++ vaers.py
@@ -1,4 +1,4 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 
 """
 Run this script on the "Daily Deaths" JSON data from
@@ -21,6 +21,7 @@ VAERS_ID = "VAERS_ID"
 VAX_DATE = "VAX_DATE"
 VAX_TYPE = "VAX_TYPE"
 ONSET_DATE = "ONSET_DATE"
+REPORTS = "REPORTS"
 
 GLOBAL_OFFSET = 1
 
@@ -49,48 +50,81 @@ XLABELS = {
 }
 
 SYMPTOM_EXP = {
+    'ageusia': 'ageusia (loss of taste)',
+    'amnesia': 'amnesia (loss of memories)',
+    'angioedema': 'angioedema (swelling under the skin)',
+    'angiogram': 'angiogram (heart x-ray)',
+    'anosmia': 'anosmia (loss of smell)',
+    'aphasia': 'aphasia (difficulty with speech or language)',
     'apnoea': 'apnoea (breathing stops while you sleep)',
     'arthralgia': 'arthralgia (joint pain)',
     'asthenia': 'asthenia (lack of energy)',
+    'atrial fibrillation': 'atrial fibrillation (irregular heart rate)',
     'cellulitis': 'cellulitis (skin infection)',
     'contusion': 'contusion (bruise)',
     'cyanosis': 'cyanosis (blue tint of the skin)',
+    'deep vein thrombosis': 'deep vein thrombosis (blood clot)',
     'dermatitis bullous': 'dermatitis bullous (blisters)',
+    'dysarthria': 'dysarthria (slurring words)',
+    'diplopia': 'diplopia (double vision)',
+    'dysgeusia': 'dysgeusia (altered perception of taste)',
     'dyskinesia': 'dyskinesia (involuntary twitching)',
+    'dyspepsia': 'dyspepsia (indigestion)',
     'dysphagia': 'dysphagia (difficulty swallowing)',
+    'dysphonia': 'dysphonia (abnormal voice)',
     'dyspnoea': 'dyspnoea (laboured breathing)',
+    'dysstasia': 'dysstasia (difficulty standing)',
+    'eye pruritus': 'eye pruritus (itchy eye)',
+    'epistaxis': 'epistaxis (nosebleed)',
     'erythema': 'erythema (redness)',
     'face oedema': 'face oedema (swelling)',
     'herpes zoster': 'herpes zoster (cold sore)',
+    'hemiparesis': 'hemiparesis (can\'t move one side of the body)',
     'hyperhidrosis': 'hyperhidrosis (excessive sweating)',
+    'hypersomnia': 'hypersomnia (daytime sleepiness)',
     'hypertension': 'hypertension (high blood pressure)',
+    'hypoacusis': 'hypoacusis (numbness)',
     'hypoaesthesia': 'hypoaesthesia (numbness)',
+    'hypoaesthesia oral': 'hypoaesthesia oral (mouth numbness)',
     'hypokinesia': 'hypokinesia (limited range of movement)',
     'hypotension': 'hypotension (low blood pressure)',
     'hypotonia': 'hypotonia (decreased muscle tone)',
+    'hypoxia': 'hypoxia (low oxygen)',
     'induration': 'induration (inflamed stiffness)',
     'injection site cellulitis': 'injection site cellulitis (skin infection)',
     'injection site erythema': 'injection site erythema (redness)',
     'injection site oedema': 'injection site oedema (swelling)',
-    'injection site pruritus': 'injection site pruritus (desire to scratch)',
+    'injection site pruritus': 'injection site pruritus (itch)',
     'injection site urticaria': 'injection site urticaria (hives)',
     'insomnia': "insomnia (can't sleep)",
+    'lacrimation increased': 'lacrimation increased (watering eyes)',
     'lethargy': 'lethargy (lack of energy)',
     'lymphadenopathy': 'lymphadenopathy (swollen lymph nodes)',
     'malaise': 'malaise (general discomfort)',
     'myalgia': 'myalgia (muscle pain)',
+    'myocardial infarction': 'myocardial infarction (heart attack)',
+    'nasopharyngitis': 'nasopharyngitis (common cold)',
+    'neuralgia': 'neuralgia (sharp pain)',
+    'ocular hyperemia': 'ocular hyperemia (red eyes)',
     'oedema': 'oedema (swelling)',
     'oedema peripheral': 'oedema peripheral (swelling)',
     'oropharyngeal pain': 'oropharyngeal pain (sore throat)',
     'pallor': 'pallor (pale appearance)',
+    'pharyngeal paraesthesia': 'pharyngeal paraesthesia (false obstruction)',
+    'photophobia': 'photophobia (light sensitivity)',
+    'paraesthesia oral': 'paraesthesia oral (pins and needles)',
     'paraesthesia': 'paraesthesia (pins and needles)',
     'pharyngitis': 'pharyngitis (sore throat)',
+    'parosmia': 'parosmia (distorted sense of smell)',
+    'petechiae': 'petechiae (pinpoint spots)',
     'presyncope': 'presyncope (going to faint)',
-    'pruritus': 'pruritus (desire to scratch)',
+    'pruritus': 'pruritus (itch)',
+    'pulmonary embolism': 'pulmonary embolism (arterial blockage, lung)',
     'pyrexia': 'pyrexia (raised body temperature)',
     'rash erythematous': 'rash erythematous (inflamed capillaries)',
     'rash macular': 'rash macular (small red spots)',
     'rash maculo-papular': 'rash maculo-papular (small raised red bumps)',
+    'rash papular': 'rash papular (bumpy rash)',
     'rash pruritic': 'rash pruritic (itchy rash)',
     'rash vesicular': 'rash vesicular (small blisters)',
     'rhinitis': 'rhinitis (cold, allergies)',
@@ -98,6 +132,9 @@ SYMPTOM_EXP = {
     'somnolence': 'somnolence (feeling sleepy)',
     'syncope': 'syncope (fainting)',
     'tachycardia': 'tachycardia (fast heartbeat)',
+    'thrombosis': 'thrombosis (blood clot in vein or artery)',
+    'tinnitus': 'tinnitus (ringing in the ears)',
+    'troponin': 'troponin (type of protein found in heart muscle)',
     'urticaria': 'urticaria (hives, rash)',
     'vasodilatation': 'vasodilatation (hot flushed skin)'
 }
@@ -146,7 +183,7 @@ def parse_vaxfreq(vax_data, vax_data_csv, detail_data, symptom_data, args):
         vax_rows = vax_data_csv
     else:
         vax_rows = vax_data_csv[vax_data_csv[VAX_TYPE].isin(args.vaxfreq)]
-    symptom_join = symptom_data.join(vax_rows.set_index(VAERS_ID), on=VAERS_ID)
+    symptom_join = vax_rows.join(symptom_data.set_index(VAERS_ID), on=VAERS_ID)
     columns = [x for x in symptom_join.keys() if re_symptoms.match(x)]
     for i, row in symptom_join[columns].iterrows():
         row = {x.lower() for x in row if isinstance(x, str)}
@@ -155,6 +192,7 @@ def parse_vaxfreq(vax_data, vax_data_csv, detail_data, symptom_data, args):
                 continue
             if len(row) == 1:
                 row.add('no other symptoms')
+        vax_data[REPORTS] = vax_data.get(REPORTS, 0) + 1
         for symptom in row:
             vax_data[symptom] = vax_data.get(symptom, 0) + 1
 
@@ -218,7 +256,7 @@ def plot_onset(vax_data, args):
     else:
         vax_onsets = range(0, days_max + 1)
 
-    plt.figure(num=1, figsize=(11, 7))
+    plt.figure(num=1, figsize=(8, 8))
 
     ymax = 0
     reports = 0
@@ -228,6 +266,9 @@ def plot_onset(vax_data, args):
     for row_i, (vax_name, _) in enumerate(sorted(vax_frequency.items(), reverse=True, key=lambda kv: kv[1])):
         if row_i > rows:
             break
+        # skip vaccines not listed
+        if args.vax and vax_name not in args.vax:
+            continue
         # get values from the row
         vax_dt = vax_data[vax_name]
         print(vax_frequency[vax_name], vax_name, "reports", min(vax_dt.keys()), "-", max(vax_dt.keys()), "days")
@@ -262,8 +303,10 @@ def plot_onset(vax_data, args):
 
         plt.plot([i + GLOBAL_OFFSET for i in x], y, label=vax_name)
 
-    title = "Adverse Reaction"
+    title = "Event"
+    fname = "vax_onset.png"
     if args.death:
+        fname = "vax_death_onset.png"
         title = "Death"
 
     plt.xscale("log")
@@ -299,6 +342,9 @@ def plot_onset(vax_data, args):
             ha_t = "center"
         plt.text(label_x + GLOBAL_OFFSET, ymax * yscale, label_t, color="blue", ha=ha_t)
 
+    plt.subplots_adjust(left=0.07, right=.985, top=0.96, bottom=0.07)
+    plt.savefig(fname, dpi=135)
+
 
 def chunk(n, chunks, lst):
     if not chunks or chunks == 1:
@@ -308,15 +354,20 @@ def chunk(n, chunks, lst):
 
 
 def plot_vaxfreq(vax_data, args):
+    reports = vax_data[REPORTS]
+    vax_data = {k: v for k, v in vax_data.items() if k != REPORTS}
+
     pos = []
     labels = []
     frequency = []
-    rows = args.n or 200
+    rows = args.n or 500
     for i, (k, v) in enumerate(sorted(vax_data.items(), reverse=True, key=lambda kv: kv[1])):
         pos.append(i)
-        labels.append(SYMPTOM_EXP.get(k, k))
+        label = SYMPTOM_EXP.get(k, k)
+        label = '{1:s} #{0:d}'.format(i+1, label)
+        labels.append(label)
         frequency.append(v)
-        if i > rows:
+        if i+1 >= rows:
             break
 
     for ci in range(args.chunks or 1):
@@ -326,7 +377,7 @@ def plot_vaxfreq(vax_data, args):
 
         pos_c = [c - pos_c[0] for c in pos_c]
 
-        plt.figure(num=ci, figsize=(8, 8))
+        plt.figure(num=ci, figsize=(8, 11))
         plt.rc('xtick', labelsize=8)
         plt.barh(pos_c, frequency_c, align='center')
 
@@ -337,12 +388,10 @@ def plot_vaxfreq(vax_data, args):
         plt.xlim(0, max(frequency) * 1.08)
         plt.ylim((max(pos_c) + .5), -0.5)
         symlist = ", ".join(args.vaxfreq)
-        title = f"VAERS {symlist:s} Vaccination Symptoms By Frequency"
-        if args.chunks and args.chunks > 1:
-            title = title + " {0:d}/{1:d}".format(ci + 1, args.chunks)
-        plt.title(title, fontsize=13)
-        plt.subplots_adjust(left=0.3, right=.98, top=0.95, bottom=0.03)
-        plt.savefig("vaxfreq{0:d}.png".format(ci + 1), dpi=200)
+        title = f"{reports:,d} {symlist:s} Unverified VAERS Reports By Symptom Frequency"
+        plt.title(title, fontsize=11)
+        plt.subplots_adjust(left=0.32, right=.985, top=0.97, bottom=0.03)
+        plt.savefig("vaxfreq{0:d}.png".format(ci + 1), dpi=300)
 
 
 def main():
@@ -353,7 +402,7 @@ def main():
     parser.add_argument('--vaxfreq', type=str, nargs="*", help="plot symptom frequency")
     parser.add_argument('--death', action="store_true", help="deaths only")
     parser.add_argument('--vax', nargs='+', help="count these vaccinations only")
-    parser.add_argument('--prevax', action="store_true", help="show pre-vaccination reports (reactions before vaccination)")
+    parser.add_argument('--prevax', action="store_true", help="show pre-vaccination reports (events before vaccination)")
     parser.add_argument('--ylog', action="store_true", help="plot Y axis in log")
     parser.add_argument('--acc', action="store_true", help="accumulate")
     parser.add_argument('stats', metavar='DATA.csv', type=str, nargs="+",
